b6089b4ce7f1ea4a1300a070c1a396e6086bbb56
hemant030406 <hemant030406@gmail.com>
2024-03-26 06:37:31 +0530

    clean up done

--- sudo.py
+++ sudo.py
@@ -3,6 +3,7 @@ import validator
 import time
 import pycosat
 from copy import deepcopy
+from math import sqrt
 rows, cols = 9, 9
 
 class SudokuSolver:
@@ -21,36 +22,10 @@ class SudokuSolver:
                 line = f.readline()
         
         self.base_cnf = self.min_one_val_cnf() + self.col_cnf() + self.row_cnf() + self.smaller_block_cnf() + self.max_one_val_cnf()
-        self.clause_map = tuple({} for _ in range(len(self.init_states)))
-        self.clause_list = tuple([] for _ in range(len(self.init_states)))
-
-    def cnf_prop_var_preproc(self, state_index: int, cnf: list[list[int]]) -> list[list[int]]:
-        self.clause_map[state_index].clear()
-        new_cnf = []
-        counter = 1
-        for clause in cnf:
-            new_clause = []
-            for propvar in clause:
-                if propvar in self.clause_map[state_index]:
-                    new_clause.append(self.clause_map[state_index][propvar])
-                elif -propvar in self.clause_map[state_index]:
-                    new_clause.append(-self.clause_map[state_index][-propvar])
-                else:
-                    if propvar < 0:
-                        neg = True
-                    else: neg = False
-                    self.clause_map[state_index][abs(propvar)] = counter
-                    self.clause_list[state_index].append(abs(propvar))
-                    counter += 1
-                    new_clause.append((counter - 1) * (-1 if neg else 1))
-            new_cnf.append(new_clause)
-        return new_cnf
      
     def solve(self, state_index: int) -> list:
         # start = time.time()
         cnf = self.base_cnf + self.gen_case_specific_cnf(state_index)
-        # print("Clauses:", len(cnf))
-        # new_cnf = self.cnf_prop_var_preproc(state_index, cnf)
         # self.generate_time += (start2 := time.time()) - start
         solved_cnf = pycosat.solve(cnf)
         # self.solve_time += time.time() - start2
@@ -60,7 +35,6 @@ class SudokuSolver:
     def print_board_from_cnf(self, state_index: int, cnf_solved: list[int]):
         board = deepcopy(self.init_states[state_index])
         for prop_var in cnf_solved:
-            # row, col, val = str(self.clause_list[state_index][prop_var - 1])
             row, col, val = str(prop_var)[0], str(prop_var)[1], str(prop_var)[2]
             board[int(row) - 1][int(col) - 1] = int(val)
         
@@ -69,12 +43,10 @@ class SudokuSolver:
 
     def gen_case_specific_cnf(self, state_index: int) -> list[list[int]]:
         cnf = []
-        # print(self.init_states[state_index])
         for i in range(rows):
             for j in range(cols):
                 if (val := self.init_states[state_index][i][j]):
                     cnf.append([int(f'{i+1}{j+1}{val}')])
-        # print(cnf)
         return cnf
 
     @staticmethod
@@ -128,19 +100,19 @@ class SudokuSolver:
     @staticmethod
     def smaller_block_cnf() -> list[list[int]]:
         cnf = []
-        for col in range(1,cols+1,3):
+        for col in range(1,cols+1,int(sqrt(cols))):
             for val in range(1,rows + 1):
-                for row in range(1,rows + 1,3):
+                for row in range(1,rows + 1,int(sqrt(rows))):
                     li = []
-                    for i in range(3):
-                        for j in range(3):
+                    for i in range(int(sqrt(rows))):
+                        for j in range(int(sqrt(cols))):
                             li.append(int(str(row + i)+str(col + j)+str(val)))
                     cnf.append(li)
         return cnf
 
 
-def main():
-    ss = SudokuSolver('p.txt')
+def main(fp: str):
+    ss = SudokuSolver(fp)
     for i in range(len(ss.init_states)):
         print("Solving: ", i)
         ans = ss.solve(i)
@@ -152,11 +124,5 @@ def main():
     # print(f"{ss.generate_time = }")
     # print(f"{ss.solve_time = }")
 
-def solve_for1():
-    ss = SudokuSolver('p.txt')
-    ss.solve(0)
-
-    # print(ans)
 if __name__ == '__main__':
-    main()
-    # solve_for1()
+    main(sys.argv[1])
